cfd7991219ffa2d98e48ddb549c082922ec583a3
ranawp <ranasaha4224#gmail.com>
2022-05-07 09:35:08 +0600

    navbar added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
